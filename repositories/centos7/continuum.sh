3ec6887aa9f6fa032d5c0972ef03f3f04b14d61c
Chonlasith Jucksriporn <chonlasith@gmail.com>
2016-07-29 22:24:44 +0700

    add message

--- repositories/centos7/continuum.sh
+++ repositories/centos7/continuum.sh
@@ -89,6 +89,7 @@ show_jenkins_secret ()
 
 install_composer ()
 {
+    echo "Installing Composer"
     sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
     sudo php composer-setup.php
